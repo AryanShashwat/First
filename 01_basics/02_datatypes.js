9f7d97b9cdd6994921bc1a232d59465376821061
AryanShashwat <dynamo3710@gmail.com>
2024-11-30 11:39:22 +0000

    Aryan

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,33 @@
+"use strict";  //after useing this js compiler will treat this whole program as newier version of nodejs
+
+// alert(3+3) this will show error because in nodejs their is different syntax for alert and it runs on web browser js 
+let run
+console.log(run)\"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Hitesh")
+
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
